e2487bfe27b7e974c2698ee3bed246f18fd6e1f0
Lchav35 <82476949+Lchav35@users.noreply.github.com>
2025-08-27 19:48:47 -0300

    Update supabaseClient.ts

--- src/lib/supabaseClient.ts
+++ src/lib/supabaseClient.ts
@@ -1,6 +1,6 @@
 import { createClient } from '@supabase/supabase-js'
 
-const supabaseUrl = 'https://YOUR_PROJECT_ID.supabase.co'
-const supabaseKey = 'YOUR_PUBLIC_ANON_KEY'
+const supabaseUrl = 'https://xrfjomztvpixfvtyrtos.supabase.co'
+const supabaseKey = 'YOUR_PUBLIC_ANON_KEY' // substitua pela sua anon key real
 
 export const supabase = createClient(supabaseUrl, supabaseKey)
