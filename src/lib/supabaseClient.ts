4578f946fe85ab0c6b4793e1224096d43be13e38
Lchav35 <82476949+Lchav35@users.noreply.github.com>
2025-08-27 19:52:47 -0300

    Update supabaseClient.ts

--- src/lib/supabaseClient.ts
+++ src/lib/supabaseClient.ts
@@ -2,5 +2,5 @@
 import { createClient } from '@supabase/supabase-js'
 
 const supabaseUrl = 'https://dkstznotepmvvidywcow.supabase.co'
-const supabaseKey = process.env.SUPABASE_KEY
+const supabaseKey = eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImRrc3R6bm90ZXBtdnZpZHl3Y293Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3NTYzMzQzMTAsImV4cCI6MjA3MTkxMDMxMH0.SZTtzwDIXfBCcF2-z3SjexGcpaG1i2C5eJtsHSDL5qU
 const supabase = createClient(supabaseUrl, supabaseKey)
