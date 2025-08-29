35aa553e47004d71947afc96e17444cade625b51
Lchav35 <82476949+Lchav35@users.noreply.github.com>
2025-08-29 19:46:18 -0300

    Update script.js

--- script.js
+++ script.js
@@ -1,2 +1,9 @@
+
+import { createClient } from '@supabase/supabase-js'
+
+const supabaseUrl = 'https://dkstznotepmvvidywcow.supabase.co'
+const supabaseKey = process.env.SUPABASE_KEY
+const supabase = createClient(supabaseUrl, supabaseKey)
+
 // Aqui você pode adicionar interações futuras
 console.log("VA Beauty Studio carregado com sucesso!");
